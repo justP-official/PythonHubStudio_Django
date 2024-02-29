4920d607e1f5b763b2c752bb08607c3d1785cd3f
justP-official <red-hot-shot@yandex.ru>
2024-02-29 16:42:12 +0300

    Ссылки для товаров

--- goods/views.py
+++ goods/views.py
@@ -14,5 +14,13 @@ def catalog(request):
     return render(request, "goods/catalog.html", context=context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'title': f'Home - {product.name}',
+        'product': product
+    }
+
+    return render(request, "goods/product.html", context=context)
