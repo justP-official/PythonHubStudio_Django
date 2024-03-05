3c23517b64ab54901a3d8a8edf15096a79e4ef9e
justP-official <red-hot-shot@yandex.ru>
2024-03-05 13:08:26 +0300

    Регистрация пользователя

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -27,8 +27,18 @@ def login(request):
     return render(request, 'users/login.html', context=context)
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+           form.save()
+           user = form.instance
+           auth.login(request=request, user=user)
+           return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
     context = {
-        'title': 'Home - Регистрация'
+        'title': 'Home - Регистрация',
+        'form': form
     }
 
     return render(request, 'users/registration.html', context=context)
@@ -41,4 +51,5 @@ def profile(request):
     return render(request, 'users/profile.html', context=context)
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
