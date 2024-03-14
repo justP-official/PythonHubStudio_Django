0e9ca60850adfa4fd3c16f6f4597111fd4048d48
justP-official <red-hot-shot@yandex.ru>
2024-03-14 12:37:49 +0300

    Создание заказов готово

--- orders/models.py
+++ orders/models.py
@@ -17,7 +17,7 @@ class OrderItemQuerySet(models.QuerySet):
 class Order(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.SET_DEFAULT, null=True, blank=True, verbose_name="Пользователь", default=None)
     created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания заказа")
-    phone_numder = models.CharField(max_length=20, verbose_name="Номер телефона")
+    phone_number = models.CharField(max_length=20, verbose_name="Номер телефона")
     requires_delivery = models.BooleanField(default=False, verbose_name="Требуется доставка")
     delivery_address = models.TextField(null=True, blank=True, verbose_name="Адрес доставки")
     payment_on_get = models.BooleanField(default=False, verbose_name="Оплата при получении")
